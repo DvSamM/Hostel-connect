b7dd9be545b41b2414824f142a06a42d08a5846e
emmanuel <eajejohnson@gmail.com>
2024-09-23 11:55:29 +0100

    little changw

---
+++ FRONTENDHOS/src/Components/Auth/SignUp/SignUp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <>
+        <div>
+            <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae laboriosam, neque animi, quibusdam asperiores velit quis molestias sint ducimus temporibus id dicta est corporis consequatur sapiente quam eveniet dolorem excepturi?</h1>
+        </div>
+    </>
+  )
+}
+
+export default SignUp
