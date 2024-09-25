a89aa668f034e73da9a3a6cd0726b12a80bbe361
Oyewole Samuel <oyewolesamuel567@gmail.com>
2024-09-25 12:19:56 +0100

    housing

--- FRONTENDHOS/src/Components/Auth/SignUp/SignUp.jsx
+++ FRONTENDHOS/src/Components/Auth/SignUp/SignUp.jsx
@@ -1,105 +1,102 @@
-import React from 'react'
-import log from "../../../assets/log.jpg"
-import { useFormik } from 'formik'
+import React, { useEffect, useState } from 'react';
+import log from "../../../assets/log.jpg";
+import { useFormik } from 'formik';
 import * as yup from "yup";
 import { Link, useNavigate } from 'react-router-dom';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
 import axios from 'axios';
 
 const Signup = () => {
-  let url = "http://localhost:1000/user/signup"
-  const navigate = useNavigate()
-    const formik = useFormik({
-        initialValues: {
-            Name: "",
-            Email: "",
-            Password: "",
-        },
+  const [message, setMessage] = useState(''); // State to hold the message
+     const url = "https://hostel-connect-4-bkc9.onrender.com/user/signin"
+  const navigate = useNavigate();
 
-        onSubmit: (values) => {
-            console.log(values);
-            axios.post(url, values ) 
-            .then((res) => {
-              console.log(res);
-              Swal.fire("SignUp Succssfully Login to proceed");
-              navigate("/user/Signin")
-            })
-            .catch((err) => {
-              console.log(err); 
-            })
-          },
-          
-          validationSchema: yup.object({
-            Email: yup.string().required("Email is required"),
-            Name: yup.string().required("Name is required"),
-            Password: yup
-            .string()
-            .required("Password is required")
-            .matches(
-              /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[\W_]).{8,}$/,
-              "Password must be at least 8 characters long, with at least one uppercase letter, one lowercase letter, one number, and one special character"
-            ), 
-            
-          }),
+  const formik = useFormik({
+    initialValues: {
+      Name: "",
+      Email: "",
+      Password: "",
+    },
+
+    onSubmit: (values) => {
+      console.log(values);
+      axios.post(url, values)
+        .then((res) => {
+          console.log(res);
+          Swal.fire("User Registered, Login to proceed");
+          navigate("/user/Signin");
+        })
+        .catch((err) => {
+          console.log(err);
+          setMessage(err.response?.data.message || 'An error occurred'); // Set error message
         });
+    },
 
-    
+    validationSchema: yup.object({
+      Email: yup.string().required("Email is required"),
+      Name: yup.string().required("Name is required"),
+      Password: yup
+        .string()
+        .required("Password is required")
+        .matches(
+          /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[\W_]).{8,}$/,
+          "Password must be at least 8 characters long, with at least one uppercase letter, one lowercase letter, one number, and one special character"
+        ),
+    }),
+  });
 
-
-
-
+  useEffect(() => {
+    if (message) {
+      setTimeout(() => {
+        setMessage('');
+      }, 3000);
+    }
+  }, [message]);
 
   return (
     <>
-    
-    <div class="wrapper login-3">
-            <div class="container d-flex" style={{marginTop:"160px"}}>
-                <div class="col-left">
-                    <div class="login-text">
-                        <h2><img src={log} alt="Logo"/></h2>
-                        <p>
-                        Experience a range of beautifully designed rooms and suites that cater to all your needs. Each room is equipped with:
-                        </p>
-                        <button className='btn'>Read More</button>
-                    </div>
-                </div>
-                <div class="col-right">
-                    <div class="login-form">
-                        <h2>SIGNUP!</h2>
-                        <form onSubmit={formik.handleSubmit}>
-                            <p>
-                                <input type="text" placeholder="Name" name='Name' onChange={formik.handleChange}/>
-                                <small>{formik.errors.Name}</small>
-                                
-                            </p>
-                            <p>
-                                <input type="email" placeholder="Email" name='Email' onChange={formik.handleChange}/>
-                                <small>{formik.errors.Email}</small>
-                                
-                            </p>
-                            <p>
-                                <input type="password" placeholder="Password" name='Password' onChange={formik.handleChange}/>
-                                <small>{formik.errors.Password}</small>
-                                
-                            </p>
-                            <p>
-                                 <button className='btn w-100 btn-primary'>Sign up</button>
-                            </p>
-                            <p>
-                               <small>Have an account?</small> <a href="">
-                                <Link to="/user/Signin">
-                                Sign In.
-                                </Link>
-                                </a>
-                            </p>
-                        </form>
-                    </div>
-                </div>
+      <div className="wrapper login-3">
+        <div className="container d-flex" style={{ marginTop: "160px" }}>
+          <div className="col-left">
+            <div className="login-text">
+              <h2><img src={log} alt="Logo" /></h2>
+              <p>
+                Experience a range of beautifully designed rooms and suites that cater to all your needs. Each room is equipped with:
+              </p>
+              <button className='btn'>Read More</button>
             </div>
+          </div>
+          <div className="col-right">
+            <div className="login-form">
+              <h2>SIGNUP!</h2>
+              {message && <p className="error-message fw-bolder text-danger fs-3">{message}!!!!!!!!</p>} 
+              <form onSubmit={formik.handleSubmit}>
+                <p>
+                  <input type="text" placeholder="Name" name='Name' onChange={formik.handleChange} />
+                  <small>{formik.errors.Name}</small>
+                </p>
+                <p>
+                  <input type="email" placeholder="Email" name='Email' onChange={formik.handleChange} />
+                  <small>{formik.errors.Email}</small>
+                </p>
+                <p>
+                  <input type="password" placeholder="Password" name='Password' onChange={formik.handleChange} />
+                  <small>{formik.errors.Password}</small>
+                </p>
+                <p>
+                  <button className='btn w-100 btn-primary'>Sign up</button>
+                </p>
+                <p>
+                  <small>Have an account?</small>
+                  <Link to="/user/Signin"> Sign In.</Link>
+                </p>
+              </form>
+            </div>
+          </div>
         </div>
-    
+      </div>
     </>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
--- FRONTENDHOS/src/Components/Auth/Signin/Signin.jsx
+++ FRONTENDHOS/src/Components/Auth/Signin/Signin.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import log from "../../../assets/log.jpg"
-import { useFormik } from 'formik'
+import React, { useState, useEffect } from 'react';
+import log from "../../../assets/log.jpg";
+import { useFormik } from 'formik';
 import * as yup from "yup";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-    
-
 const Signin = () => {
+    const [message, setMessage] = useState(''); // State for messages
+    const url = "https://hostel-connect-4-bkc9.onrender.com/user/signin"
+
     const formik = useFormik({
         initialValues: {
             Email: "",
@@ -16,74 +17,78 @@ const Signin = () => {
 
         onSubmit: (values) => {
             console.log(values);
-          },
-          
-          validationSchema: yup.object({
+            axios.post(url, values)
+                .then((res) => {
+                    console.log(res);
+                    localStorage.setItem('token', res.data.token); 
+                })
+                .catch((err) => {
+                    console.log(err);
+                    setMessage(err.response?.data.message || 'An error occurred'); // Se
+                });
+        },
+
+        validationSchema: yup.object({
             Email: yup.string().required("Email is required"),
             Password: yup
-            .string()
-            .required("Password is required")
-            .matches(
-              /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[\W_]).{8,}$/,
-              "Password must be at least 8 characters long, with at least one uppercase letter, one lowercase letter, one number, and one special character"
-            ), 
-            
-          }),
-        });
+                .string()
+                .required("Password is required")
+                .matches(
+                    /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[\W_]).{8,}$/,
+                    "Password must be at least 8 characters long, with at least one uppercase letter, one lowercase letter, one number, and one special character"
+                ),
+        }),
+    });
 
-    
+    useEffect(() => {
+        if (message) {
+            setTimeout(() => {
+            setMessage('');
+            }, 3000); 
+        }
+    }, [message]);
 
-
-
-
-
-  return (
-    <>
-    
-    <div class="wrapper login-3">
-            <div class="container d-flex" style={{marginTop:"160px"}}>
-                <div class="col-left">
-                    <div class="login-text">
-                        <h2><img src={log} alt="Logo"/></h2>
-                        <p>
-                        Experience a range of beautifully designed rooms and suites that cater to all your needs. Each room is equipped with:
-                        </p>
-                        <a class="btn" href="">Read More</a>
+    return (
+        <>
+            <div className="wrapper login-3">
+                <div className="container d-flex" style={{ marginTop: "160px" }}>
+                    <div className="col-left">
+                        <div className="login-text">
+                            <h2><img src={log} alt="Logo" /></h2>
+                            <p>
+                                Experience a range of beautifully designed rooms and suites that cater to all your needs. Each room is equipped with:
+                            </p>
+                            <button className='btn'>Read More</button>
+                        </div>
                     </div>
-                </div>
-                <div class="col-right">
-                    <div class="login-form">
-                        <h2>Login</h2>
-                        <form onSubmit={formik.handleSubmit}>
-                            <p>
-                                <input type="email" placeholder="Email" name='Email' onChange={formik.handleChange}/>
-                                <small>{formik.errors.Email}</small>
-                                
-                            </p>
-                            <p>
-                                <input type="password" placeholder="Password" name='Password' onChange={formik.handleChange}/>
-                                <small>{formik.errors.Password}</small>
-                                
-                            </p>
-                            <p>
-                            <button className='btn w-100 btn-primary'>Sign in</button>
-                            </p>
-                            <p>
-                                <a href="">Forget password?</a>
-                               <small>Don't have an account?</small> <a href="">
-                                <Link to="/user/Signup">
-                                Create an account.
-                                </Link>
-                                </a>
-                            </p>
-                        </form>
+                    <div className="col-right">
+                        <div className="login-form">
+                            <h2>Login</h2>
+                            {message && <p className="error-message">{message}</p>} {/* Display the message here */}
+                            <form onSubmit={formik.handleSubmit}>
+                                <p>
+                                    <input type="email" placeholder="Email" name='Email' onChange={formik.handleChange} />
+                                    <small>{formik.errors.Email}</small>
+                                </p>
+                                <p>
+                                    <input type="password" placeholder="Password" name='Password' onChange={formik.handleChange} />
+                                    <small>{formik.errors.Password}</small>
+                                </p>
+                                <p>
+                                    <button className='btn w-100 btn-primary'>Sign in</button>
+                                </p>
+                                <p>
+                                    <a href="">Forget password?</a>
+                                    <small>Don't have an account?</small>
+                                    <Link to="/user/Signup"> Create an account.</Link>
+                                </p>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    
-    </>
-  )
+        </>
+    );
 }
 
-export default Signin
+export default Signin;
