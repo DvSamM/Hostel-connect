1864c1240657d79433ae74faba95597b09b16931
Oyewole Samuel <oyewolesamuel567@gmail.com>
2024-09-25 10:56:37 +0100

    housing

--- BACKENDHOS/routes/user.route.js
+++ BACKENDHOS/routes/user.route.js
@@ -3,11 +3,7 @@ const router = express.Router();
 
 const {signup, signin} = require("../Controller/user.controller")
 
-<<<<<<< HEAD
-router.get("/signin", fetchAll)
-=======
 router.post("/signin", signin)
->>>>>>> 2af24cb78f82f4ff0fdbf4ffeaca80224ed73b66
 router.post("/signup", signup)
 
 module.exports = router
