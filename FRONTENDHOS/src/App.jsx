5937983d5c087d36daf16dacd171b7d5ab0ee3b9
Oyewole Samuel <oyewolesamuel567@gmail.com>
2024-09-26 11:20:08 +0100

    housing

--- FRONTENDHOS/src/App.jsx
+++ FRONTENDHOS/src/App.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react'
-import Layout from './Components/Home/Layout/Layout'
-import Landing from './Components/Home/Landing/Landing'
-import { Route, Routes, useNavigate } from 'react-router-dom'
-import Signin from './Components/Auth/Signin/Signin'
-import Signup from './Components/Auth/SignUp/SignUp'
-import Layoutdash from './Components/Dashboard/Layoutdash'
-import Dashboard from './Components/Dashboard/Dashboard'
-import NotFound from './Components/404/Notfound'
+import React, { useState } from 'react';
+import Layout from './Components/Home/Layout/Layout';
+import Landing from './Components/Home/Landing/Landing';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Signin from './Components/Auth/Signin/Signin';
+import Signup from './Components/Auth/SignUp/SignUp';
+import Layoutdash from './Components/Dashboard/Layoutdash';
+import Dashboard from './Components/Dashboard/Dashboard';
+import NotFound from './Components/404/Notfound';
 
 const App = () => {
   const [isSignedUp, setIsSignedUp] = useState(false);
-  const Navigate = useNavigate()
+
   return (
     <>
-    <Routes>
-    <Route path='*' element={<NotFound/>}/>
+      <Routes>
+      <Route path='*' element={<NotFound/>}/>
     <Route path='/' element={<Layout/>}>
     <Route path='/' element={<Landing/>} />
     <Route path='/home' element={<Landing/>} />
@@ -22,17 +22,18 @@ const App = () => {
 
 
     <Route path='/' element={<Layout/>}>
-    <Route path='/user/Signup' element={<Signup/>} />
+    <Route path='/user/Signup' element={<Signup setIsSignedUp={setIsSignedUp} />} />
     <Route path='/user/Signin' element={<Signin/>} />
     </Route>
-
-    <Route path="/user/Signup" element={<Signup setIsSignedUp={setIsSignedUp} />} />
-      <Route path="/user/dashboard" element={<Layoutdash />}>
-        <Route path="/user/dashboard" element={isSignedUp ? <Dashboard /> : <Navigate to="/user/signup" />} />
-      </Route>
-    </Routes>
+        <Route path="/user/dashboard" element={<Layoutdash />}>
+          <Route 
+            index 
+            element={isSignedUp ? <Dashboard /> : <Navigate to="/user/signup" />} 
+          />
+        </Route>
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
