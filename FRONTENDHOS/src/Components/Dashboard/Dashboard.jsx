cda07f964a011e75b61e0fde54b9cc347a665428
Oyewole Samuel <oyewolesamuel567@gmail.com>
2024-09-30 14:42:04 +0100

    housing

--- FRONTENDHOS/src/Components/Dashboard/Dashboard.jsx
+++ FRONTENDHOS/src/Components/Dashboard/Dashboard.jsx
@@ -51,50 +51,13 @@ const Dashboard = () => {
                   </div>
                 </div>
               </div>
-
-              <div className="card shadow-sm">
-                <img 
-                  src={hotel.imageUrl} 
-                  className="bd-placeholder-img card-img-top" 
-                  alt={hotel.name} 
-                  style={{ width: '100%', height: '225px', objectFit: 'cover' }} 
-                />
-                <div className="card-body">
-                  <p className="card-text">{hotel.name}</p>
-                  <p className="card-text">{hotel.description}</p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small className="text-body-secondary">9 mins</small>
-                  </div>
-                </div>
-              </div>
-              
-              <div className="card shadow-sm">
-                <img 
-                  src={hotel.imageUrl} 
-                  className="bd-placeholder-img card-img-top" 
-                  alt={hotel.name} 
-                  style={{ width: '100%', height: '225px', objectFit: 'cover' }} 
-                />
-                <div className="card-body">
-                  <p className="card-text">{hotel.name}</p>
-                  <p className="card-text">{hotel.description}</p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small className="text-body-secondary">9 mins</small>
-                  </div>
-                </div>
-              </div>
             </div>
           ))}
         </div>
       </div>
+          
+
+
     </>
   );
 };
